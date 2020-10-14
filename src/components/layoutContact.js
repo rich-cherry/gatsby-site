bfb496351fffef6777ce2b75c8f79acad7a2fa1d
rich-cherry <rich.m.cherry@gmail.com>
2020-10-13 23:42:51 -0600

    added animations to hero

---
+++ src/components/layoutContact.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './header'
+import Footer from './footer'
+import layoutContactStyles from './layoutContact.module.scss'
+
+
+export default function LayoutContact(props) {
+    return (
+        <div className={layoutContactStyles.mainContainer}>
+            <div className={layoutContactStyles.container}>
+                <div className={layoutContactStyles.content}>
+                    <Header />
+                    {props.children}
+                </div>
+                <Footer />
+            </div>
+        </div >
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,21 @@
 import React from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
-import '../styles/index.scss'
 import PageTransition from 'gatsby-v2-plugin-page-transitions'
+import '../styles/index.scss'
+import indexStyles from './index.module.scss'
 
 
 export default function IndexPage() {
   return (
-    <div>
-      <Layout>
-        <PageTransition>
+    <Layout>
+      <PageTransition>
+        <div className={indexStyles.delay}>
           <Head title="Home" />
-          <h1>Hello.</h1>
-          <h2>I'm Rich, a developer.</h2>
-        </PageTransition>
-      </Layout>
-    </div>
+          <h1 className={indexStyles.animatePopIn}>We translate business goals into design strategy in order to</h1>
+          <h2 className={indexStyles.animatePopIn}>I'm Rich, a developer.</h2>
+        </div>
+      </PageTransition>
+    </Layout>
   )
 }
