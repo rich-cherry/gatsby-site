753cdd0cfd8a0a24c88943ba48b2572d930b31e1
rich-cherry <rich.m.cherry@gmail.com>
2020-10-24 10:36:36 -0400

    added photos to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,4 @@
 import React from "react"
-import styled from "styled-components";
 import { graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../layouts/layout"
@@ -15,9 +14,9 @@ export default function About(data) {
     <StaticQuery
       query={graphql`
                 query {
-                    rc: file(relativePath: { eq: "images/rc_photo6.jpg"}) {
+                    rc: file(relativePath: { eq: "images/rc-black-white.png"}) {
                         childImageSharp {
-                            fixed(width: 190, quality: 100){
+                            fixed(width: 300, quality: 100){
                                 ...GatsbyImageSharpFixed
                             }
                         }
@@ -28,80 +27,82 @@ export default function About(data) {
         <Layout>
           <PageTransition>
             <Head title="About" />
-            <div>
-              <h1>About me</h1>
-              <h3>Hi, I'm Rich -</h3>
-              <p>I'm a developer with 6+ years of experience creating and managing eCommerce websites on Shopify and Shopify Plus, which includes everything from small theme tweaks and customizations to full, headless, custom-built sites from scratch.</p>
-              <p>Need help? Let's get together and build something great.</p>
-            </div>
             <div className={aboutStyles.container}>
               <div>
-                <p>I most frequently work with:</p>
-                <ul>
-                  <li>CSS3 <FontAwesomeIcon icon={faCss3Alt} /></li>
-                  <li>HTML5 <FontAwesomeIcon icon={faHtml5} /></li>
-                  <li>GatsbyJS <FontAwesomeIcon icon={faReact} /></li>
-                  <li>Git <FontAwesomeIcon icon={faGithub} /></li>
-                  <li>GraphQL</li>
-                  <li>Javascript <FontAwesomeIcon icon={faJs} /></li>
-                  <li>NodeJS <FontAwesomeIcon icon={faNodeJs} /></li>
-                  <li>Liquid <FontAwesomeIcon icon={faShopify} /></li>
-                  <li>ReactJS <FontAwesomeIcon icon={faReact} /></li>
-                  <li>Sass <FontAwesomeIcon icon={faSass} /></li>
-                </ul>
-              </div>
-              <div>
-                <p>I occasionally work with:</p>
-                <ul>
-                  <li>Adobe Illustrator</li>
-                  <li>Adobe Photoshop</li>
-                  <li>Bootstrap <FontAwesomeIcon icon={faBootstrap} /></li>
-                  <li>Figma <FontAwesomeIcon icon={faFigma} /></li>
-                  <li>Ruby <FontAwesomeIcon /></li>
-                  <li>Ruby on Rails</li>
-                  <li>SketchUp</li>
-                  <li>SQL</li>
-                </ul>
-              </div>
-              <div>
-                <p>Favorite CMS / eCommerce / Hosting Platforms & Tools:</p>
-                <ul>
-                  <li>Contenful</li>
-                  <li>Klaviyo</li>
-                  <li>Netlify</li>
-                  <li>Shopify <FontAwesomeIcon icon={faShopify} /></li>
-                  <li>Whimsical</li>
-                  <li>Zapier</li>
-                </ul>
-              </div>
-              <div>
-                <p>Previous eCommerce work has included:</p>
-                <ul>
-                  <li>Accounting Integration</li>
-                  <li>Custom User Features</li>
-                  <li>Inventory Management</li>
-                  <li>Marketing
+                <h1>About me</h1>
+                <h3>Hi, I'm Rich -</h3>
+                <p>I'm a developer with 6+ years of experience creating and managing eCommerce websites on Shopify and Shopify Plus, which includes everything from small theme tweaks and customizations to full, headless, custom-built sites from scratch.</p>
+                <p>Need help? Let's get together and build something great.</p>
+                <div>
+                  <p>I most frequently work with:</p>
+                  <ul>
+                    <li>CSS3 <FontAwesomeIcon icon={faCss3Alt} /></li>
+                    <li>HTML5 <FontAwesomeIcon icon={faHtml5} /></li>
+                    <li>GatsbyJS <FontAwesomeIcon icon={faReact} /></li>
+                    <li>Git <FontAwesomeIcon icon={faGithub} /></li>
+                    <li>GraphQL</li>
+                    <li>Javascript <FontAwesomeIcon icon={faJs} /></li>
+                    <li>NodeJS <FontAwesomeIcon icon={faNodeJs} /></li>
+                    <li>Liquid <FontAwesomeIcon icon={faShopify} /></li>
+                    <li>ReactJS <FontAwesomeIcon icon={faReact} /></li>
+                    <li>Sass <FontAwesomeIcon icon={faSass} /></li>
+                  </ul>
+                </div>
+                <div>
+                  <p>I occasionally work with:</p>
+                  <ul>
+                    <li>Adobe Illustrator</li>
+                    <li>Adobe Photoshop</li>
+                    <li>Bootstrap <FontAwesomeIcon icon={faBootstrap} /></li>
+                    <li>Figma <FontAwesomeIcon icon={faFigma} /></li>
+                    <li>Ruby <FontAwesomeIcon /></li>
+                    <li>Ruby on Rails</li>
+                    <li>SketchUp</li>
+                    <li>SQL</li>
+                  </ul>
+                </div>
+                <div>
+                  <p>Favorite CMS / eCommerce / Hosting Platforms & Tools:</p>
+                  <ul>
+                    <li>Contenful</li>
+                    <li>Klaviyo</li>
+                    <li>Netlify</li>
+                    <li>Shopify <FontAwesomeIcon icon={faShopify} /></li>
+                    <li>Whimsical</li>
+                    <li>Zapier</li>
+                  </ul>
+                </div>
+                <div>
+                  <p>Previous eCommerce work has included:</p>
+                  <ul>
+                    <li>Accounting Integration</li>
+                    <li>Custom User Features</li>
+                    <li>Inventory Management</li>
+                    <li>Marketing
              <ul>
-                      <li>Klaviyo</li>
-                      <li>Constant Contact</li>
-                      <li>Google Analytics Integration</li>
-                      <li>Product Creation and Copy</li>
-                      <li>Shogun</li>
-                      <li>Shopify Launchpad</li>
-                      <li>Shopify Scripts</li>
-                    </ul>
-                  </li>
-                  <li>Migration from other platforms</li>
-                  <li>Theme Design and Development</li>
-                  <li>Transportation Management & Fulfillment
+                        <li>Klaviyo</li>
+                        <li>Constant Contact</li>
+                        <li>Google Analytics Integration</li>
+                        <li>Product Creation and Copy</li>
+                        <li>Shogun</li>
+                        <li>Shopify Launchpad</li>
+                        <li>Shopify Scripts</li>
+                      </ul>
+                    </li>
+                    <li>Migration from other platforms</li>
+                    <li>Theme Design and Development</li>
+                    <li>Transportation Management & Fulfillment
              <ul>
-                      <li>Ground</li>
-                      <li>LTL</li>
-                    </ul>
-                  </li>
-                  <li>UX / UI Optimization</li>
-                </ul>
+                        <li>Ground</li>
+                        <li>LTL</li>
+                      </ul>
+                    </li>
+                    <li>UX / UI Optimization</li>
+                  </ul>
+                </div>
               </div>
+
+
               <Img fixed={data.rc.childImageSharp.fixed} />
 
             </div>
