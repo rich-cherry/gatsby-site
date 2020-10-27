4b864f2bda2745ff50da9ac150b6a4d338db891e
rich-cherry <rich.m.cherry@gmail.com>
2020-10-27 11:41:35 -0400

    fixed responsiveness issues

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../layouts/layout"
 import Head from "../components/head"
 import PageTransition from 'gatsby-v2-plugin-page-transitions'
+import thanksStyles from './thanks.module.scss'
 
 
 export default function IndexPage() {
@@ -9,8 +10,10 @@ export default function IndexPage() {
         <Layout>
             <PageTransition>
                 <Head title="Thanks" />
-                <h1>Thanks for dropping a line!</h1>
-                <h2>I'll be in touch!</h2>
+                <div className={thanksStyles.text}>
+                    <h1>Thanks for dropping a line.</h1>
+                    <h2>I'll be in touch!</h2>
+                </div>
 
             </PageTransition>
         </Layout>
