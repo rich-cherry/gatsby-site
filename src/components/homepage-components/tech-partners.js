793804875a1597bdc2e166038f8fcf0f3778d66d
rich-cherry <rich.m.cherry@gmail.com>
2021-04-04 15:17:31 -0400

    updated fonts

--- src/components/homepage-components/tech-partners.js
+++ src/components/homepage-components/tech-partners.js
@@ -4,9 +4,9 @@ import { graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
 export default function TechPartners() {
-    return (
-        <StaticQuery
-            query={graphql`
+  return (
+    <StaticQuery
+      query={graphql`
                 query {
                     shopify: file(relativePath: { eq: "images/brands/shopify.png"}) {
                         childImageSharp {
@@ -14,7 +14,7 @@ export default function TechPartners() {
                                 ...GatsbyImageSharpFixed_withWebp
                             }
                         }
-                    }  
+                    }
                     klaviyo: file(relativePath: { eq: "images/brands/klaviyo.png"}) {
                         childImageSharp {
                             fixed(width: 200){
@@ -35,68 +35,68 @@ export default function TechPartners() {
                                 ...GatsbyImageSharpFixed_withWebp
                             }
                         }
-                    }      
+                    }
                 }
             `}
-            render={data => (
-                <div className={techPartnerStyles.container}>
-                    <div className={techPartnerStyles.techPartnerContainer}>
-                        <div className={techPartnerStyles.techPartnerText}
-                            data-sal="fade"
-                            data-sal-duration="2000"
-                            data-sal-delay="200"
-                            data-sal-easing="ease">
-                            <h1>Tech Partners</h1>
-                            <hr />
-                            <div className={techPartnerStyles.partnerText}>
-                                <p><strong>Shopify:</strong> eCommerce Platform</p>
-                                <p><strong>Klaviyo:</strong> Marketing Automation</p>
-                                <p><strong>Netlify:</strong> Developer Platform</p>
-                                <p><strong>Contentful:</strong> Content Management</p>
-                            </div>
-                        </div>
-                        <div className={techPartnerStyles.techPartnerGrid}>
-                            <a href="https://www.shopify.com" target="_blank"
-                                rel="noreferrer noopener" alt="Shopify"
-                                data-sal="fade"
-                                data-sal-duration="2000"
-                                data-sal-delay="200"
-                                data-sal-easing="ease"
-                            >
-                                <Img fixed={data.shopify.childImageSharp.fixed} />
-                            </a>
-                            <a href="https://www.klaviyo.com/partner/signup?utm_source=0013o00002TrPtG&utm_medium=partner"
-                                target="_blank"
-                                rel="noreferrer noopener"
-                                alt="Klavio"
-                                data-sal="fade"
-                                data-sal-duration="2000"
-                                data-sal-delay="400"
-                                data-sal-easing="ease"
-                            >
-                                <Img fixed={data.klaviyo.childImageSharp.fixed} />
-                            </a>
-                            <a href="https://www.netlify.com/" target="_blank" rel="noreferrer noopener" alt="Netlify"
-                                data-sal="fade"
-                                data-sal-duration="2000"
-                                data-sal-delay="200"
-                                data-sal-easing="ease"
-                            >
-                                <Img fixed={data.netlify.childImageSharp.fixed} />
-                            </a>
-                            <a href="https://www.contentful.com/" target="_blank" rel="noreferrer noopener" alt="Contentful"
-                                data-sal="fade"
-                                data-sal-duration="2000"
-                                data-sal-delay="400"
-                                data-sal-easing="ease"
-                            >
-                                <Img fixed={data.contentful.childImageSharp.fixed} />
-                            </a>
-                        </div>
-                    </div>
-                </div >
-            )}
-        />
-    )
+      render={data => (
+        <div className={techPartnerStyles.container}>
+          <div className={techPartnerStyles.techPartnerContainer}>
+            <div className={techPartnerStyles.techPartnerText}
+              data-sal="fade"
+              data-sal-duration="2000"
+              data-sal-delay="200"
+              data-sal-easing="ease">
+              <h1>Tech Partners</h1>
+              <hr />
+              <div className={techPartnerStyles.partnerText}>
+                <p><strong>Shopify:</strong> eCommerce Platform</p>
+                <p><strong>Klaviyo:</strong> Marketing Automation</p>
+                <p><strong>Netlify:</strong> Developer Platform</p>
+                <p><strong>Contentful:</strong> Content Management</p>
+              </div>
+            </div>
+            <div className={techPartnerStyles.techPartnerGrid}>
+              <a href="https://www.shopify.com" target="_blank"
+                rel="noreferrer noopener" alt="Shopify"
+                data-sal="fade"
+                data-sal-duration="2000"
+                data-sal-delay="200"
+                data-sal-easing="ease"
+              >
+                <Img fixed={data.shopify.childImageSharp.fixed} />
+              </a>
+              <a href="https://www.klaviyo.com/partner/signup?utm_source=0013o00002TrPtG&utm_medium=partner"
+                target="_blank"
+                rel="noreferrer noopener"
+                alt="Klavio"
+                data-sal="fade"
+                data-sal-duration="2000"
+                data-sal-delay="400"
+                data-sal-easing="ease"
+              >
+                <Img fixed={data.klaviyo.childImageSharp.fixed} />
+              </a>
+              <a href="https://www.netlify.com/" target="_blank" rel="noreferrer noopener" alt="Netlify"
+                data-sal="fade"
+                data-sal-duration="2000"
+                data-sal-delay="200"
+                data-sal-easing="ease"
+              >
+                <Img fixed={data.netlify.childImageSharp.fixed} />
+              </a>
+              <a href="https://www.contentful.com/" target="_blank" rel="noreferrer noopener" alt="Contentful"
+                data-sal="fade"
+                data-sal-duration="2000"
+                data-sal-delay="400"
+                data-sal-easing="ease"
+              >
+                <Img fixed={data.contentful.childImageSharp.fixed} />
+              </a>
+            </div>
+          </div>
+        </div >
+      )}
+    />
+  )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function IndexPage() {
         <PageTransition>
           <div className={indexStyles.hero}>
             <Head title="Developer" />
-            <h1 className={indexStyles.animatePopIn}>US-based Shopify Developer offering tweaks, custom features, migrations, site builds from scratch, and custom private apps.</h1>
+            <h1 className={indexStyles.animatePopIn}>Shopify Developer offering tweaks, custom features, migrations, site builds from scratch, and custom private apps.</h1>
             <h2 className={indexStyles.animatePopIn}>Need help with a project?</h2>
             <Button>hire me today</Button>
             <hr />
